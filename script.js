35041ef58e109108c480e4adf110deb1ca02f496
AzizTeqnite123 <azizteqnite@gmail.com>
2025-04-26 22:38:15 +0500

    initial

--- script.js
+++ script.js
@@ -61,34 +61,32 @@ var swiper = new Swiper('.cstmSlider', {
 
 // VIDEO-PLAY-BTN
 
-let currentVideo = null;
+// let currentVideo = null;
 
-document.querySelectorAll('.playBtnVd').forEach(function (playButton) {
-    playButton.addEventListener('click', function () {
-        var video = this.closest('.glrVdHldr').querySelector('video');
-        
-        // Pause the currently playing video if there is one
-        if (currentVideo && currentVideo !== video) {
-            currentVideo.pause();
-            currentVideo.controls = false;
-            currentVideo.closest('.glrVdHldr').querySelector('.playBtnVd').style.display = 'block';
-        }
+// document.querySelectorAll('.playBtnVd').forEach(function (playButton) {
+//     playButton.addEventListener('click', function () {
+//         var video = this.closest('.glrVdHldr').querySelector('video');
 
-        // Play or pause the clicked video
-        if (video.paused) {
-            video.play();
-            video.controls = true;
-            this.style.display = 'none';
-            currentVideo = video; // Set the current video to the one that's playing
-        } else {
-            video.pause();
-            video.controls = false;
-            this.style.display = 'block';
-            currentVideo = null; // Reset the current video if paused
-        }
-    });
-});
+//         // Pause the currently playing video if there is one
+//         if (currentVideo && currentVideo !== video) {
+//             currentVideo.pause();
+//             currentVideo.controls = false;
+//             currentVideo.closest('.glrVdHldr').querySelector('.playBtnVd').style.display = 'block';
+//         }
 
-
+//         // Play or pause the clicked video
+//         if (video.paused) {
+//             video.play();
+//             video.controls = true;
+//             this.style.display = 'none';
+//             currentVideo = video; // Set the current video to the one that's playing
+//         } else {
+//             video.pause();
+//             video.controls = false;
+//             this.style.display = 'block';
+//             currentVideo = null; // Reset the current video if paused
+//         }
+//     });
+// });
 
 // VIDEO-PLAY-BTN
