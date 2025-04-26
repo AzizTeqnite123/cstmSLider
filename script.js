43fe9837b820fac8d2d65ff3290d950974fe4690
AzizTeqnite123 <azizteqnite@gmail.com>
2025-04-26 22:28:30 +0500

    inital

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// VIDEO-POPUP
+
 Fancybox.bind("[data-fancybox]", {
     Thumbs: {
         autoStart: true,
@@ -11,16 +13,16 @@ Fancybox.bind("[data-fancybox]", {
     },
 });
 
+// VIDEO-POPUP
+
+
+// SLIDER-SCRIPT
 
 var swiper = new Swiper('.cstmSlider', {
     centeredSlides: true,
     slidesPerView: 3,
     loop: true,
-    simulateTouch: true,
-    touchStartPreventDefault: true,
     allowTouchMove: false,
-    grabCursor: true,
-    slideToClickedSlide: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -54,21 +56,39 @@ var swiper = new Swiper('.cstmSlider', {
     }
 });
 
+// SLIDER-SCRIPT
 
-// Select all play buttons
+
+// VIDEO-PLAY-BTN
+
+let currentVideo = null;
+
 document.querySelectorAll('.playBtnVd').forEach(function (playButton) {
     playButton.addEventListener('click', function () {
         var video = this.closest('.glrVdHldr').querySelector('video');
-        // var controls = video.controls;
+        
+        // Pause the currently playing video if there is one
+        if (currentVideo && currentVideo !== video) {
+            currentVideo.pause();
+            currentVideo.controls = false;
+            currentVideo.closest('.glrVdHldr').querySelector('.playBtnVd').style.display = 'block';
+        }
 
+        // Play or pause the clicked video
         if (video.paused) {
             video.play();
             video.controls = true;
             this.style.display = 'none';
+            currentVideo = video; // Set the current video to the one that's playing
         } else {
             video.pause();
             video.controls = false;
             this.style.display = 'block';
+            currentVideo = null; // Reset the current video if paused
         }
     });
 });
+
+
+
+// VIDEO-PLAY-BTN
